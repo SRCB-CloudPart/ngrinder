8a96a24073c04a86698587d258aa6016e70d1776
juno.yoon <juno.yoon@samsung.com>
2015-08-21 10:03:46 +0900

    increase ulimit

--- ngrinder-controller/src/main/resources/agent_autoscale_script/docker-init.sh
+++ ngrinder-controller/src/main/resources/agent_autoscale_script/docker-init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e -x
 yum install wget -y
+# increase file open limit
+echo "root soft  nofile 40000" >> /etc/security/limits
+echo "root hard  nofile 40000" >> /etc/security/limits
+ulimit -n 40000
 sed -i s/'Defaults    requiretty'/'#Defaults    requiretty'/ /etc/sudoers                  # allow root access without tty.
 wget -qO- https://get.docker.com/ | sh >> /tmp/ngrinder_agent_provision.log 2>&1           # install docker
 #Below line is for Amazon generic unix system, if the VM is based on Ubuntu, should check /etc/default/docker DOCKER_OPTS
